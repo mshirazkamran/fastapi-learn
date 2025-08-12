d9b405a42fa0acf937ecec50d57b1bd1e76feb02
mshirazkamran <blogger31625@gmail.com>
2025-08-12 20:20:59 +0500

    update: Body() in annotated

--- src/learn2.py
+++ src/learn2.py
@@ -1,5 +1,6 @@
+from turtle import title
 from urllib.parse import uses_fragment
-from fastapi import FastAPI, Query
+from fastapi import Body, FastAPI, Path, Query
 from pydantic import AfterValidator
 
 from models import User, Option
@@ -38,5 +39,30 @@ async def check_user(id: Annotated[int | None, Query(ge=1,le=30), AfterValidator
 
 #2. you can also input list (multiple values) in query param
     # refer to https://fastapi.tiangolo.com/tutorial/query-params-str-validations/#query-parameter-list-multiple-values
+
+
+#3. You can use Path for the path params just as you use Query for query params
+
+@app.get("/users/check/{id}")
+async def user_check_id(id: Annotated[int, Path(title="This is a an id", gt=10)]):
+    pass
+
+
+# You can add multiple body parameters to your path operation function,
+# even though a request can only have a single body.
+
+# But FastAPI will handle it, give you the correct data in your function, 
+# and validate and document the correct schema in the path operation.
+
+# You can also declare singular values to be received as part of the body.
+# Ex:
+@app.post("/endpoint/user")
+async def endpoint(user: User, importance: Annotated[int, Body()]):
+    pass
+
+# And you can instruct FastAPI to embed the body in a key even when there is only a single parameter declared.
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Annotated[User, Body(embed=True)]):
+    pass
 
 
